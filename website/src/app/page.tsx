14686c9726a8e57de765066aaef0e159d71b4d20
chritycodx <62725473+chxrxtycodx@users.noreply.github.com>
2024-10-25 17:30:56 -0400

    Added header

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,6 +1,12 @@
-import Image from "next/image";
 
-export default function Home() {
-  return 
-    
+import Header from "@/Components/Header/Header.tsx"; 
+import styles from "./header.style.css";
+
+
+export default function Page() {
+  return (
+    <div className={styles.header}>
+      <Header />
+    </div>
+  );
 }
